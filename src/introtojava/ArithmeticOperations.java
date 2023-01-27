1001c7f908aa08b58698d6fb69577a9d35300f3c
Salma Issa <sal8653001@gmail.com>
2023-01-27 09:28:16 -0500

    first commit

---
+++ src/introtojava/ArithmeticOperations.java
@@ -0,0 +1,15 @@
+package introtojava;
+
+public class ArithmeticOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int age;
+	age = 9;
+			System.out.println (age * 2);
+	}
+
+}
+
+5%2
